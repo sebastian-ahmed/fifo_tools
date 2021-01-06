db01d5c0d68fa5665b36e18a8a57abedf0664da8
sebastian-ahmed <76592277+sebastian-ahmed@users.noreply.github.com>
2021-01-05 23:16:09 -0600

    Refactor: Add a simple sim kernel/event queue

--- fifo_pkg/FifoSimulator.py
+++ fifo_pkg/FifoSimulator.py
@@ -8,10 +8,12 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESSED OR IMPLIED.
 # See the License for the specific language governing permissions and limitations under the License.
 
+import threading
 import concurrent.futures
 import random
 import math
 import numpy as np
+import time
 
 from fifo_pkg.Fifo import Fifo
 
@@ -42,7 +44,61 @@ class FifoSimulator(object):
         else:
             self._fifo.bulk_pushes(1) # Always assume one entry in the FIFO before we start the sim
 
-    def producer_thread(self):
+        # We create a pair of events per thread to communicate with the sim kernel. The first
+        # event in each list is used to signal blocking, the second is used to signal end of the thread
+        self._kernelEvents = {
+            'e_producer' : threading.Event(),
+            'e_consumer' : threading.Event()
+        }
+
+        self._maxqlen = 0 # Debug variable to track maximum size of sim kernel event queue
+        self._pendq = [] # Pended event queue
+        self._threadCount = 0 # Number of active sim kernel threads
+
+        # Thread synchronization lock (bound)
+        self.__lock = threading.Lock()
+
+        # Flag to reflect that all pushes are done which allows consumer thread to no longer
+        # block and just finish consuming
+        self._pushesDone = False
+
+    def threadStart(self):
+        self.__lock.acquire()
+        self._threadCount += 1
+        self.__lock.release()
+
+    def threadEnd(self):
+        self.__lock.acquire()
+        self._threadCount -= 1
+        self.__lock.release()
+
+    def threadPend(self,ev):
+        '''
+        This method places a blocking thread's event into a pend queue
+        '''
+        self.__lock.acquire()
+        self._pendq.append(ev)
+        self.__lock.release()
+
+    def kernel_thread(self):
+        '''
+        This thread implements a simple simulator kernel which sequences thread events
+        based on the pend queue order. As pended events are popped from the key, they
+        are set to unblock each thread.
+        '''
+        time.sleep(1) # Block time for threads to register active state
+        print("Starting kernel thread...")
+        while self._threadCount > 0:
+            if len(self._pendq) > 0:
+                if len(self._pendq) > self._maxqlen:
+                    self._maxqlen = len(self._pendq)
+                cur_ev = self._pendq.pop(0)
+                assert not cur_ev.is_set(), "Event queue ERROR, active event found"
+                cur_ev.set()
+                cur_ev.clear()
+
+    def producer_thread(self,ev):
+        self.threadStart()
         print("Started Fifo producer thread...")
         rem_pl = self._pl_size - self._init_level
         while rem_pl>0 and not self._fifo.error:
@@ -51,16 +107,28 @@ class FifoSimulator(object):
                 rem_pl -= 1
             else:
                 self._fifo.wnop()
+            self.threadPend(ev)
+            ev.wait()
+        self._pushesDone = True # Tell consumer, we are done pushing
+        self.threadEnd()
 
-    def consumer_thread(self):
+    def consumer_thread(self,ev):
+        self.threadStart()
         print("Started Fifo consumer thread...")
         rem_pl = self._pl_size
         while rem_pl>0 and not self._fifo.error:
-            if FifoSimulator.getRandomBool([self._rrate,self._wrate])==True:
+            # Don't call the randomizer if producer is done as this skewes the effective
+            # bandwidth ratio metrics
+            if self._pushesDone or FifoSimulator.getRandomBool([self._rrate,self._wrate])==True:
                 self._fifo.pop()
                 rem_pl -= 1
             else:
                 self._fifo.rnop()
+            # Only pend if the producer thread is still active
+            if not self._pushesDone:
+                self.threadPend(ev)
+                ev.wait()
+        self.threadEnd()
 
     def calcDepth(self):
         '''
@@ -82,10 +150,10 @@ class FifoSimulator(object):
         else:
             print("Running simulation...\n")
 
-            producer_fhandle = lambda : self.producer_thread()
-            consumer_fhandle = lambda : self.consumer_thread()
-
-            threadList = [producer_fhandle, consumer_fhandle]
+            kernel_fcall   = lambda : self.kernel_thread() 
+            producer_fcall = lambda : self.producer_thread(ev=self._kernelEvents['e_producer'])
+            consumer_fcall = lambda : self.consumer_thread(ev=self._kernelEvents['e_consumer'])
+            threadList = [kernel_fcall,producer_fcall, consumer_fcall]
 
             with concurrent.futures.ThreadPoolExecutor(max_workers=None) as executor:
                 _ = {executor.submit(x): x for x in threadList}
